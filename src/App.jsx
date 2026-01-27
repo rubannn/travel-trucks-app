59f05432fc6baf3a07a19cd7ca3361e1a16311c9
m.ruban <m.ruban@testportal.gov.ua>
2026-01-27 09:25:11 +0100

    add images and sprite

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import viteLogo from '/favicon.svg'
+import viteLogo from '/favicon.png'
 import './App.css'
 
 function App() {
