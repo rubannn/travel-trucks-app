a37ea2b2a1bcd3ce458d6242ffabf73c0de5b3da
m.ruban <m.ruban@testportal.gov.ua>
2026-01-27 11:15:12 +0100

    refactor Loader component to improve ProgressBar structure

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,9 +1,16 @@
+import { ProgressBar } from 'react-loader-spinner'
 import css from "./Loader.module.css"
 
 const Loader = () => {
   return (
     <div className={css.loader}>
-
+      <ProgressBar
+        visible={true}
+        ariaLabel="progress-bar-loading"
+        width="100"
+        color="rgb(228, 72, 72)"
+        barColor="rgb(228, 72, 72)"
+      />
     </div>
   )
 }
