b7e0bedb61870d07049f572fde82ea0fe58d4acb
m.ruban <m.ruban@testportal.gov.ua>
2026-01-27 11:14:07 +0100

    extract baseURL variable in axios instance configuration

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,4 +1,7 @@
 import axios from "axios";
+
+const baseURL = "https://66b1f8e71ca8ad33d4f5f63e.mockapi.io";
+
 export const instance = axios.create({
-  baseURL: "https://66b1f8e71ca8ad33d4f5f63e.mockapi.io",
+  baseURL: baseURL,
 });
