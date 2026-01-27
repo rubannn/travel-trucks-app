a7fcd697bca3bf3af1aa14d8ed06305db52aab1b
m.ruban <m.ruban@testportal.gov.ua>
2026-01-27 13:10:56 +0100

    update styles

--- src/components/DetailedInfo/DetailedInfo.jsx
+++ src/components/DetailedInfo/DetailedInfo.jsx
@@ -1,11 +1,12 @@
 import { useSelector } from 'react-redux';
-import css from './DetailedInfo.module.css'
 import { selectTruck } from '../../redux/truck/selectors';
 import icons from '../../assets/sprite.svg';
 import clsx from "clsx"
 import { Navigate, NavLink, Route, Routes } from 'react-router-dom';
 import TruckFeatures from '../TruckFeatures/TruckFeatures';
 import TruckReviews from '../TruckReviews/TruckReviews';
+
+import css from './DetailedInfo.module.css'
 
 const activeLink = ({ isActive }) => {
   return clsx(css.details_link, isActive && css.active);
@@ -15,41 +16,48 @@ const DetailedInfo = () => {
   const truck = useSelector(selectTruck);
   return (
     <>
-      <h2 className={css.name_title}>{truck.name}</h2>
-      <div className={css.rating_wrapper}>
-        <svg className={css.icon} width="16" height="16">
-          <use href={`${icons}#icon-Rating`} />
-        </svg>
-        <p className={css.reviews}>{truck.rating} ({truck.reviews ? truck.reviews.length : 0} Reviews)</p>
-        <svg className={css.icon} width="20" height="20">
-          <use href={`${icons}#Map`} />
-        </svg> {truck.location}
+      <div className={css.header}>
+        <div className={css.nameRow}>
+          <h2 className={css.name_title}>{truck.name}</h2>
+
+          <div className={css.rating_wrapper}>
+            <svg className={css.icon} width="16" height="16">
+              <use href={`${icons}#icon-Rating`} />
+            </svg>
+            <p className={css.reviews}>{truck.rating} ({truck.reviews ? truck.reviews.length : 0} Reviews)</p>
+            <svg className={css.icon} width="20" height="20">
+              <use href={`${icons}#Map`} />
+            </svg> {truck.location}
+          </div>
+
+          <p className={css.name_title}>{`€ ${Number(truck.price).toFixed(2)}`}</p>
+        </div>
+
+        {truck.gallery.length > 0 ? (
+          <ul className={css.gallery}>
+            {truck.gallery.map((item, index) => (
+              <li key={index}>
+                <img className={css.photo} src={item.thumb} alt={`Gallery image ${index + 1}`} />
+              </li>
+            ))}
+          </ul>
+        ) : (
+          <p>No images available.</p>
+        )}
+        <p className={css.item_description}>{truck.description}</p>
       </div>
-      <p className={css.name_title}>{`€ ${Number(truck.price).toFixed(2)}`}</p>
 
-      {truck.gallery.length > 0 ? (
-        <ul className={css.gallery}>
-          {truck.gallery.map((item, index) => (
-            <li key={index}>
-              <img className={css.photo} src={item.thumb} alt={`Gallery image ${index + 1}`} />
-            </li>
-          ))}
+      <div className={css.footer}>
+        <ul className={css.details_list}>
+          <li className={css.details_item}><NavLink className={activeLink} to="features">Features</NavLink></li>
+          <li className={css.details_item}><NavLink className={activeLink} to="reviews">Reviews</NavLink></li>
         </ul>
-      ) : (
-        <p>No images available.</p>
-      )}
-      <p className={css.item_description}>{truck.description}</p>
-
-      <ul className={css.details_list}>
-        <li className={css.details_item}><NavLink className={activeLink} to="features">Features</NavLink></li>
-        <li className={css.details_item}><NavLink className={activeLink} to="reviews">Reviews</NavLink></li>
-      </ul>
-      <Routes>
-        <Route path="" element={<Navigate to="features" />} />
-        <Route path="features" element={<TruckFeatures />}></Route>
-        <Route path="reviews" element={<TruckReviews />}></Route>
-      </Routes>
-
+        <Routes>
+          <Route path="" element={<Navigate to="features" />} />
+          <Route path="features" element={<TruckFeatures />}></Route>
+          <Route path="reviews" element={<TruckReviews />}></Route>
+        </Routes>
+      </div>
     </>
   )
 }
--- src/pages/DetailsPage/DetailsPage.jsx
+++ src/pages/DetailsPage/DetailsPage.jsx
@@ -1,32 +1,35 @@
 
 import { Helmet } from "react-helmet-async"
-import { useDispatch} from "react-redux";
+import { useDispatch } from "react-redux";
 import { useParams } from "react-router-dom";
 import { useEffect } from "react";
 import { fetchTruckDetails } from "../../redux/truck/operations";
-import css from "./DetailsPage.module.css"
 import DetailedInfo from "../../components/DetailedInfo/DetailedInfo";
 import { Toaster } from "react-hot-toast";
 
+import css from "./DetailsPage.module.css";
+
 const DetailsPage = () => {
-     const dispatch = useDispatch();
-     const { id } = useParams();
-     
-  useEffect(() => {
-   dispatch(fetchTruckDetails(id));
-  }, [dispatch,id]);
-   
-  return (
-      <>
-          <Helmet>
-              <title>Details Page</title>
-          </Helmet>
-          <Toaster position="top-center" reverseOrder={false} />
-          <section className={css.detailed_container} >
-               <DetailedInfo/> 
-          </section>
-      </>
-  )
+    const dispatch = useDispatch();
+    const { id } = useParams();
+
+    useEffect(() => {
+        dispatch(fetchTruckDetails(id));
+    }, [dispatch, id]);
+
+    return (
+        <>
+            <Helmet>
+                <title>Details Page</title>
+            </Helmet>
+            <Toaster position="top-center" reverseOrder={false} />
+            <div className={css.wrapper} >
+                <div className={css.detailed_container} >
+                    <DetailedInfo />
+                </div>
+            </div>
+        </>
+    )
 }
 
 export default DetailsPage
